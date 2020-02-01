c48c61ac886aaa829055cfc42485d41a9e585692
Enrique Devars <e.devars.dev@gmail.com>
2020-02-01 02:14:26 -0600

    :rocket: First hello world with Django

---
+++ platzigram/urls.py
@@ -0,0 +1,13 @@
+"""Platzigram URL module"""
+
+from django.urls import path
+from django.http import HttpResponse
+
+
+def hello_world(reques):
+    return HttpResponse('Hello world')
+
+
+urlpatterns = [
+    path('hello-world/', hello_world),
+]
