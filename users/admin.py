63fdcdebe3f13c6bb75d49aee3180ed04cf542a6
Enrique Devars <e.devars.dev@gmail.com>
2020-02-04 10:50:30 -0600

    :sparkles: Adding users to the DB and creating view to dashboard admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from users.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user', 'phone_number')
+    list_editable = ('website', 'picture')
+    search_fields = (
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number')
+
+    list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
