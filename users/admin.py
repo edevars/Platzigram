6bfbeb685105880f382eae5d0ffbb1d1e60091e9
Enrique Devars <e.devars.dev@gmail.com>
2020-02-04 11:26:47 -0600

    :sparkles: Extending user view with the profile class

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
+# Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+# Utils
 from users.models import Profile
 
 
@@ -15,3 +20,42 @@ class ProfileAdmin(admin.ModelAdmin):
         'phone_number')
 
     list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),)
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography'),
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInline(admin.StackedInline):
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
