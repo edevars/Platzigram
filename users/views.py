d151c3d6e91ee626badb449a2a8df090be12cb5c
Enrique Devars <e.devars.dev@gmail.com>
2020-02-06 12:32:03 -0600

    :sparkles: Adding signup view and template

--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
+# Django
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+# Exception
+from django.db.utils import IntegrityError
+
+# Utils
+from users.models import Profile
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('feed')
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -16,6 +27,34 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid user or password'})
 
     return render(request, 'users/login.html')
+
+
+def sign_up_view(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.email = request.POST['email']
+            user.save()
+
+            profile = Profile(user=user)
+            profile.save()
+
+            login(request, user)
+            return redirect('feed')
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username already exists'})
+
+    return render(request, 'users/signup.html')
 
 
 @login_required
