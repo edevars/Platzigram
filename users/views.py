7477ae2c48c480ff3c0c9e1ca418ac8f7ed3f321
Enrique Devars <e.devars.dev@gmail.com>
2020-02-06 13:24:10 -0600

    :sparkles: Adding update profile

--- users/views.py
+++ users/views.py
@@ -9,10 +9,38 @@ from django.db.utils import IntegrityError
 
 # Utils
 from users.models import Profile
+from users.forms import ProfileForm
 
 
 def update_profile(request):
-    return render(request, 'users/update_profile.html')
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('feed')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
 
 
 def login_view(request):
