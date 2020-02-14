9190406c383784c4d493efdb67546615e9ad87b8
Enrique Devars <e.devars.dev@gmail.com>
2020-02-13 19:30:14 -0600

    :sparkles: Adding autogenerated form

--- users/views.py
+++ users/views.py
@@ -4,16 +4,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-# Exception
-from django.db.utils import IntegrityError
-
-# Utils
-from users.models import Profile
-from users.forms import ProfileForm
+# Forms
+from users.forms import ProfileForm, SignupForm
 
 
 def update_profile(request):
-    """Update a user's profile view."""
     profile = request.user.profile
 
     if request.method == 'POST':
@@ -62,31 +57,25 @@ def login_view(request):
 
 
 def sign_up_view(request):
-
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            data = form.save()
+            user = authenticate(
+                request, username=data['username'], password=data['password'])
+            if user:
+                login(request, user)
+                return redirect('feed')
+            else:
+                return redirect('login')
+    else:
+        form = SignupForm()
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-            user.first_name = request.POST['first_name']
-            user.last_name = request.POST['last_name']
-            user.email = request.POST['email']
-            user.save()
-
-            profile = Profile(user=user)
-            profile.save()
-
-            login(request, user)
-            return redirect('feed')
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username already exists'})
-
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
